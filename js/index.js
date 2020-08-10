cd03339b1078bf9205b654283b5d1a11563c05af
Zang KING <zangar9805@gmail.com>
2020-08-10 21:14:44 +0600

    added folder js, additional.css, fixed menu, pizza size, slider

---
+++ js/index.js
@@ -0,0 +1,41 @@
+var pizzas = document.querySelectorAll(".pizza")
+
+// console.log()
+
+function pizzaSize(p) {
+    for (var pizza of p) {
+        pizza.querySelector(".product__size").children[0].addEventListener("click", ()=>{
+            pizza.querySelector(".product__img").children[0].style.width = "100%";
+            pizza.querySelector(".product__size").children[0].classList.add("active")
+            pizza.querySelector(".product__size").children[1].classList.remove("active")
+            pizza.querySelector(".product__size").children[2].classList.remove("active")
+        })
+    
+        pizza.querySelector(".product__size").children[1].addEventListener("click", ()=>{
+            pizza.querySelector(".product__img").children[0].style.width = "85%";
+            pizza.querySelector(".product__size").children[0].classList.remove("active")
+            pizza.querySelector(".product__size").children[1].classList.add("active")
+            pizza.querySelector(".product__size").children[2].classList.remove("active")
+        })
+    
+        pizza.querySelector(".product__size").children[2].addEventListener("click", ()=>{
+            pizza.querySelector(".product__img").children[0].style.width = "70%";
+            pizza.querySelector(".product__size").children[0].classList.remove("active")
+            pizza.querySelector(".product__size").children[1].classList.remove("active")
+            pizza.querySelector(".product__size").children[2].classList.add("active")
+        })
+    }
+}
+
+jQuery(function($) {
+    $(window).scroll(function(){
+        if($(this).scrollTop()>80){
+            $('#head__fixed').addClass('fixed');
+        }
+        else if ($(this).scrollTop()<80){
+            $('#head__fixed').removeClass('fixed');
+        }
+    });
+});
+
+pizzaSize(pizzas);
